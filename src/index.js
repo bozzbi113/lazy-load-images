71f38904704944f2e3d60727e8d0ac8330f2c361
khanhtp <khanh.tuong@dinovative.com>
2018-11-19 14:32:07 +0700

    abc

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './components/App';
 
 ReactDOM.render(
+
+  
   <App url="https://github.com/completejavascript/lazy-loading-image-demo" />, 
   document.getElementById('root')
 );
